8ab7c599eb70fd81a6cbefe4477c7d715eac5b61
varunnair2001 <64598835+varunnair2001@users.noreply.github.com>
2023-06-15 15:28:52 +0530

    Add files via upload

---
+++ JS/popup.js
@@ -0,0 +1,35 @@
+$('#popup_button').click(function() {
+    $('.pop-up').addClass('open');
+});
+
+$('.pop-up .close').click(function() {
+    $('.pop-up').removeClass('open');
+});
+$('#popup_button1').click(function() {
+    $('.pop-up1').addClass('open');
+});
+
+$('.pop-up1 .close1').click(function() {
+    $('.pop-up1').removeClass('open');
+});
+$('#SPIZNET').click(function() {
+    $('.pop-up2').addClass('open');
+});
+
+$('.pop-up2 .close2').click(function() {
+    $('.pop-up2').removeClass('open');
+});
+$('#BLAZERUN').click(function() {
+    $('.pop-up3').addClass('open');
+});
+
+$('.pop-up3 .close3').click(function() {
+    $('.pop-up3').removeClass('open');
+});
+$('#popup_button4').click(function() {
+    $('.pop-up4').addClass('open');
+});
+
+$('.pop-up4 .close4').click(function() {
+    $('.pop-up4').removeClass('open');
+});
